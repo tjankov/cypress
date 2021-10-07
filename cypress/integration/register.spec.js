60d1f596b6f3eaf2298b4928f4775faae233d2d2
Tamara <tamy.ns@gmail.com>
2021-10-07 09:11:56 +0200

    domaci

--- cypress/integration/register.spec.js
+++ cypress/integration/register.spec.js
@@ -9,29 +9,62 @@
         cy.get ('a[href="/register"]').click();   
     });
 
-    it ("login with valid data", () => {
+//POSITIVE TEST CASES
+
+    it ("register with valid credentials", () => {
         cy.wait (2000);
         cy.get ('input[id="first-name"]').type('Tamara');
         cy.get ('input[id="last-name"]').type('Jankov');
-        cy.get ('input[id="email"]').type('tamarajankov@1qa1.com');
+        cy.get ('input[id="email"]').type('tamarajankov@1tttestqa.com');
         cy.get ('input[id="password"]').type('test1234');
         cy.get ('input[id="password-confirmation"]').type('test1234');
         cy.get ('input[type="checkbox"]').check();
         cy.get ('button[class="btn btn-custom"]').click();
     })
 
-    it ("all empty fields",() =>{
+    it ("logout", () => {
+        cy.get ('a[role="button "]').click();
+    })
+
+    it ("register with valid credentials in cyrillic", () => {
+        cy.wait (2000);
+        cy.get ('a[href="/register"]').click();
+        cy.get ('input[id="first-name"]').type('Тамара');
+        cy.get ('input[id="last-name"]').type('Јанков');
+        cy.get ('input[id="email"]').type('tamarajankov@2tttestqa.com');
+        cy.get ('input[id="password"]').type('тест1234');
+        cy.get ('input[id="password-confirmation"]').type('тест1234');
+        cy.get ('input[type="checkbox"]').check();
+        cy.get ('button[class="btn btn-custom"]').click();
+    })
+
+    it ("register with valid credentials - double First Name", () => {
+        cy.wait (2000);
+        cy.get ('a[role="button "]').click();
+        cy.get ('a[href="/register"]').click();
+        cy.get ('input[id="first-name"]').type('Ella-Rose');
+        cy.get ('input[id="last-name"]').type('Jankov);
+        cy.get ('input[id="email"]').type('tamarajankov@3tttestqa.com');
+        cy.get ('input[id="password"]').type('test1234');
+        cy.get ('input[id="password-confirmation"]').type('test1234');
+        cy.get ('input[type="checkbox"]').check();
+        cy.get ('button[class="btn btn-custom"]').click();
+    })
+
+//NEGATIVE TEST CASES
+
+    it ("register with all empty fields",() =>{
         cy.get ('a[role="button "]').click();
         cy.get ('a[href="/register"]').click();  
         cy.get ('button[class="btn btn-custom"]').click();
     })
 
-    it ("check only terms",() =>{
+    it ("register with only checked terms",() =>{
         cy.get ('input[type="checkbox"]').check();
         cy.get ('button[class="btn btn-custom"]').click();
     })
 
-    it ("all spaces",() =>{
+    it ("register with all spaces",() =>{
         cy.wait (2000);
         cy.get ('input[id="first-name"]').clear().type(' ');
         cy.get ('input[id="last-name"]').clear().type(' ');
@@ -42,88 +75,53 @@
         cy.get ('button[class="btn btn-custom"]').click();
     })
 
-    it ("too much characters in First Name",() =>{
-        cy.wait (2000);
-        cy.get ('input[id="first-name"]').clear().type('3FIcmfG54nfeaLNiIr8Mh8VECrcJcpLpbUfZ44vUx3xqJWMPl4bWV398OOxJBOvMlWTIchAXZy7TbdF50fdeiboAwHyKCxhMjVJm1X8XknbIa06NWmL7J30ttosX0SS6wORL2upPBapOmeFDtOK32aHUI6Jwu6z9hRrAiDgHkssy5WqLmQPFdiTzz2Yo8WPjziNcEilU3PMuUas0R0NsBMBZuwUzf93wJMfmH525n9mJj2rFqz3O0TlX3xm31k6F')
-        cy.get ('input[id="last-name"]').clear().type('Jankov');
-        cy.get ('input[id="email"]').clear().type('tamarajankov@1qa2.com');
-        cy.get ('input[id="password"]').clear().type('test1234');
-        cy.get ('input[id="password-confirmation"]').clear().type('test1234');
-        cy.get ('input[type="checkbox"]').check();
-        cy.get ('button[class="btn btn-custom"]').click();
-    })
-
-    it ("empty First Name",() =>{
+    it ("register with empty First Name field",() =>{
         cy.wait (2000);
         cy.get ('input[id="first-name"]').clear();
         cy.get ('input[id="last-name"]').clear().type('Jankov');
-        cy.get ('input[id="email"]').clear().type('tamarajankov@1qa3.com');
+        cy.get ('input[id="email"]').clear().type('tamarajankov@atestqa1.com');
         cy.get ('input[id="password"]').clear().type('test1234'); 
         cy.get ('input[id="password-confirmation"]').clear().type('test1234');
         cy.get ('input[type="checkbox"]').check();
         cy.get ('button[class="btn btn-custom"]').click();
     })
 
-
-
-    it ("too much characters in Last Name",() =>{
+//more then 255 characters
+    it ("register with long First Name",() =>{
         cy.wait (2000);
-        cy.get ('input[id="first-name"]').clear().type('Tamara');
-        cy.get ('input[id="last-name"]').clear().type('3FIcmfG54nfeaLNiIr8Mh8VECrcJcpLpbUfZ44vUx3xqJWMPl4bWV398OOxJBOvMlWTIchAXZy7TbdF50fdeiboAwHyKCxhMjVJm1X8XknbIa06NWmL7J30ttosX0SS6wORL2upPBapOmeFDtOK32aHUI6Jwu6z9hRrAiDgHkssy5WqLmQPFdiTzz2Yo8WPjziNcEilU3PMuUas0R0NsBMBZuwUzf93wJMfmH525n9mJj2rFqz3O0TlX3xm31k6F') 
-        cy.get ('input[id="email"]').clear().type('tamarajankov@1qa4.com');
+        cy.get ('input[id="first-name"]').clear().type('3FIcmfG54nfeaLNiIr8Mh8VECrcJcpLpbUfZ44vUx3xqJWMPl4bWV398OOxJBOvMlWTIchAXZy7TbdF50fdeiboAwHyKCxhMjVJm1X8XknbIa06NWmL7J30ttosX0SS6wORL2upPBapOmeFDtOK32aHUI6Jwu6z9hRrAiDgHkssy5WqLmQPFdiTzz2Yo8WPjziNcEilU3PMuUas0R0NsBMBZuwUzf93wJMfmH525n9mJj2rFqz3O0TlX3xm31k6F')
+        cy.get ('input[id="last-name"]').clear().type('Jankov');
+        cy.get ('input[id="email"]').clear().type('tamarajankov@atestqa2.com');
         cy.get ('input[id="password"]').clear().type('test1234');
         cy.get ('input[id="password-confirmation"]').clear().type('test1234');
         cy.get ('input[type="checkbox"]').check();
         cy.get ('button[class="btn btn-custom"]').click();
     })
 
-
-    it ("empty Last Name",() =>{
+    it ("register with empty Last Name field",() =>{
         cy.wait (2000);
         cy.get ('input[id="first-name"]').clear().type('Tamara');
         cy.get ('input[id="last-name"]').clear();
-        cy.get ('input[id="email"]').clear().type('tamarajankov@1qa5.com');
+        cy.get ('input[id="email"]').clear().type('tamarajankov@atestqa3.com');
         cy.get ('input[id="password"]').clear().type('test1234'); 
         cy.get ('input[id="password-confirmation"]').clear().type('test1234');
         cy.get ('input[type="checkbox"]').check();
         cy.get ('button[class="btn btn-custom"]').click();
     })
 
-
-    it ("@ not included in email", () => {
+//more then 255 characters
+    it ("register with long Last Name",() =>{
         cy.wait (2000);
         cy.get ('input[id="first-name"]').clear().type('Tamara');
-        cy.get ('input[id="last-name"]').clear().type('Jankov');
-        cy.get ('input[id="email"]').clear().type('tamarajankov1qa1.com');
+        cy.get ('input[id="last-name"]').clear().type('3FIcmfG54nfeaLNiIr8Mh8VECrcJcpLpbUfZ44vUx3xqJWMPl4bWV398OOxJBOvMlWTIchAXZy7TbdF50fdeiboAwHyKCxhMjVJm1X8XknbIa06NWmL7J30ttosX0SS6wORL2upPBapOmeFDtOK32aHUI6Jwu6z9hRrAiDgHkssy5WqLmQPFdiTzz2Yo8WPjziNcEilU3PMuUas0R0NsBMBZuwUzf93wJMfmH525n9mJj2rFqz3O0TlX3xm31k6F') 
+        cy.get ('input[id="email"]').clear().type('tamarajankov@atestqa4.com');
         cy.get ('input[id="password"]').clear().type('test1234');
         cy.get ('input[id="password-confirmation"]').clear().type('test1234');
         cy.get ('input[type="checkbox"]').check();
         cy.get ('button[class="btn btn-custom"]').click();
     })
 
-    it ("full stop not included in email", () => {
-        cy.wait (2000);
-        cy.get ('input[id="first-name"]').clear().type('Tamara');
-        cy.get ('input[id="last-name"]').clear().type('Jankov');
-        cy.get ('input[id="email"]').clear().type('tamarajankov@1qa1com');
-        cy.get ('input[id="password"]').clear().type('test1234');
-        cy.get ('input[id="password-confirmation"]').clear().type('test1234');
-        cy.get ('input[type="checkbox"]').check();
-        cy.get ('button[class="btn btn-custom"]').click();
-    })
-
-    it ("com not included in email", () => {
-        cy.wait (2000);
-        cy.get ('input[id="first-name"]').clear().type('Tamara');
-        cy.get ('input[id="last-name"]').clear().type('Jankov');
-        cy.get ('input[id="email"]').clear().type('tamarajankov@1qa1.');
-        cy.get ('input[id="password"]').clear().type('test1234');
-        cy.get ('input[id="password-confirmation"]').clear().type('test1234');
-        cy.get ('input[type="checkbox"]').check();
-        cy.get ('button[class="btn btn-custom"]').click();
-    })
-
-    it ("empty email",() =>{
+    it ("register with empty email field",() =>{
         cy.wait (2000);
         cy.get ('input[id="first-name"]').clear('Tamara');
         cy.get ('input[id="last-name"]').clear().type('Jankov');
@@ -134,24 +132,68 @@
         cy.get ('button[class="btn btn-custom"]').click();
     })
 
-    it ("invalid password format", () => {
+    it ("register with missing @ in email", () => {
         cy.wait (2000);
         cy.get ('input[id="first-name"]').clear().type('Tamara');
         cy.get ('input[id="last-name"]').clear().type('Jankov');
-        cy.get ('input[id="email"]').clear().type('tamarajankov@1qa6.com');
-        cy.get ('input[id="password"]').clear().type('testtest');
-        cy.get ('input[id="password-confirmation"]').clear().type('testtest');
+        cy.get ('input[id="email"]').clear().type('tamarajankovtestqa.com');
+        cy.get ('input[id="password"]').clear().type('test1234');
+        cy.get ('input[id="password-confirmation"]').clear().type('test1234');
         cy.get ('input[type="checkbox"]').check();
         cy.get ('button[class="btn btn-custom"]').click();
     })
 
-    it ("not enough characters in password", () => {
+    it ("register with with missing full stop in email", () => {
         cy.wait (2000);
         cy.get ('input[id="first-name"]').clear().type('Tamara');
         cy.get ('input[id="last-name"]').clear().type('Jankov');
-        cy.get ('input[id="email"]').clear().type('tamarajankov@1qa7.com');
-        cy.get ('input[id="password"]').clear().type('test123');
-        cy.get ('input[id="password-confirmation"]').clear().type('test123');
+        cy.get ('input[id="email"]').clear().type('tamarajankov@testqacom');
+        cy.get ('input[id="password"]').clear().type('test1234');
+        cy.get ('input[id="password-confirmation"]').clear().type('test1234');
+        cy.get ('input[type="checkbox"]').check();
+        cy.get ('button[class="btn btn-custom"]').click();
+    })
+
+    it ("register with with missing com in email", () => {
+        cy.wait (2000);
+        cy.get ('input[id="first-name"]').clear().type('Tamara');
+        cy.get ('input[id="last-name"]').clear().type('Jankov');
+        cy.get ('input[id="email"]').clear().type('tamarajankov@testqa.');
+        cy.get ('input[id="password"]').clear().type('test1234');
+        cy.get ('input[id="password-confirmation"]').clear().type('test1234');
+        cy.get ('input[type="checkbox"]').check();
+        cy.get ('button[class="btn btn-custom"]').click();
+    })
+
+    it ("register with with missing first part in email", () => {
+        cy.wait (2000);
+        cy.get ('input[id="first-name"]').clear().type('Tamara');
+        cy.get ('input[id="last-name"]').clear().type('Jankov');
+        cy.get ('input[id="email"]').clear().type('@testqa.com');
+        cy.get ('input[id="password"]').clear().type('test1234');
+        cy.get ('input[id="password-confirmation"]').clear().type('test1234');
+        cy.get ('input[type="checkbox"]').check();
+        cy.get ('button[class="btn btn-custom"]').click();
+    })
+
+    it ("register with with double @ in email", () => {
+        cy.wait (2000);
+        cy.get ('input[id="first-name"]').clear().type('Tamara');
+        cy.get ('input[id="last-name"]').clear().type('Jankov');
+        cy.get ('input[id="email"]').clear().type('tamarajankov@@testqa.com');
+        cy.get ('input[id="password"]').clear().type('test1234');
+        cy.get ('input[id="password-confirmation"]').clear().type('test1234');
+        cy.get ('input[type="checkbox"]').check();
+        cy.get ('button[class="btn btn-custom"]').click();
+    })
+
+    it ("register with with more than one @ in email", () => {
+        cy.wait (2000);
+        cy.get ('input[id="first-name"]').clear().type('Tamara');
+        cy.get ('input[id="last-name"]').clear().type('Jankov');
+        cy.get ('input[id="email"]').clear().type('tamara@jankov@testqa.com');
+        cy.get ('input[id="password"]').clear().type('test1234');
+        cy.get ('input[id="password-confirmation"]').clear().type('test1234');
         cy.get ('input[type="checkbox"]').check();
         cy.get ('button[class="btn btn-custom"]').click();
     })
@@ -160,52 +202,75 @@
         cy.wait (2000);
         cy.get ('input[id="first-name"]').clear().type('Tamara');
         cy.get ('input[id="last-name"]').clear().type('Jankov');
-        cy.get ('input[id="email"]').clear().type('tamarajankov@1qa8.com');
+        cy.get ('input[id="email"]').clear().type('tamarajankov@atestqa5.com');
         cy.get ('input[id="password"]').clear(); 
         cy.get ('input[id="password-confirmation"]').clear().type('test1234');
         cy.get ('input[type="checkbox"]').check();
         cy.get ('button[class="btn btn-custom"]').click();
     })
 
-    it ("empty password confirmation",() =>{
+//8 letters
+    it ("register with invalid password format", () => {
         cy.wait (2000);
         cy.get ('input[id="first-name"]').clear().type('Tamara');
         cy.get ('input[id="last-name"]').clear().type('Jankov');
-        cy.get ('input[id="email"]').clear().type('tamarajankov@1qa9.com');
+        cy.get ('input[id="email"]').clear().type('tamarajankov@atestqa6.com');
+        cy.get ('input[id="password"]').clear().type('testtest');
+        cy.get ('input[id="password-confirmation"]').clear().type('testtest');
+        cy.get ('input[type="checkbox"]').check();
+        cy.get ('button[class="btn btn-custom"]').click();
+    })
+
+//7 characters
+    it ("register with not enough characters in password", () => {
+        cy.wait (2000);
+        cy.get ('input[id="first-name"]').clear().type('Tamara');
+        cy.get ('input[id="last-name"]').clear().type('Jankov');
+        cy.get ('input[id="email"]').clear().type('tamarajankov@atestqa7.com');
+        cy.get ('input[id="password"]').clear().type('test123');
+        cy.get ('input[id="password-confirmation"]').clear().type('test123');
+        cy.get ('input[type="checkbox"]').check();
+        cy.get ('button[class="btn btn-custom"]').click();
+    })
+
+    it ("register with empty password confirmation",() =>{
+        cy.wait (2000);
+        cy.get ('input[id="first-name"]').clear().type('Tamara');
+        cy.get ('input[id="last-name"]').clear().type('Jankov');
+        cy.get ('input[id="email"]').clear().type('tamarajankov@atestqa8.com');
         cy.get ('input[id="password"]').clear().type('test1234');
         cy.get ('input[id="password-confirmation"]').clear();
         cy.get ('input[type="checkbox"]').check();
         cy.get ('button[class="btn btn-custom"]').click();
     })
 
-    it ("no mach password confirmation",() =>{
+    it ("register with no matching password and password confirmation",() =>{
         cy.wait (2000);
         cy.get ('input[id="first-name"]').clear().type('Tamara');
         cy.get ('input[id="last-name"]').clear().type('Jankov');
-        cy.get ('input[id="email"]').clear().type('tamarajankov@1qa10.com');
+        cy.get ('input[id="email"]').clear().type('tamarajankov@atestqa9.com');
         cy.get ('input[id="password"]').clear().type('test1234');
         cy.get ('input[id="password-confirmation"]').clear().type('1234test');
         cy.get ('input[type="checkbox"]').check();
         cy.get ('button[class="btn btn-custom"]').click();
     })
 
-
-    it ("email allready exist", () => {
+    it ("register with already registered email", () => {
         cy.wait (2000);
         cy.get ('input[id="first-name"]').clear().type('Tamara');
         cy.get ('input[id="last-name"]').clear().type('Jankov');
-        cy.get ('input[id="email"]').clear().type('tamarajankov@1qa1.com');
+        cy.get ('input[id="email"]').clear().type('tamarajankov@testqa.com');
         cy.get ('input[id="password"]').clear().type('test1234');
         cy.get ('input[id="password-confirmation"]').clear().type('test1234');
         cy.get ('input[type="checkbox"]').check();
         cy.get ('button[class="btn btn-custom"]').click();
     })  
 
-    it ("uncheck terms", () => {
+    it ("register with unchecked terms", () => {
         cy.wait (2000);
         cy.get ('input[id="first-name"]').clear().type('Tamara')
         cy.get ('input[id="last-name"]').clear().type('Jankov')
-        cy.get ('input[id="email"]').clear().type('tamarajankov@1qa11.com')
+        cy.get ('input[id="email"]').clear().type('tamarajankov@atestqa11.com')
         cy.get ('input[id="password"]').clear().type('test1234') 
         cy.get ('input[id="password-confirmation"]').clear().type('test1234')
         cy.get ('input[type="checkbox"]').uncheck();
